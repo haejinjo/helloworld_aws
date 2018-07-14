2dabbd2a02d1aa2546dfc1d51ceb3529c70cb601
Haejin <professionalhaejin@gmail.com>
2018-07-14 04:41:28 -0700

    oops wrong path

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express();
 app.listen(3000, () => "Listening on 3000!");
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + './index.html')
+    res.sendFile(__dirname + '/index.html')
 });
