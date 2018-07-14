5ef2ab9af9e4add209a6390063d0edcb1ef31b1b
Haejin <professionalhaejin@gmail.com>
2018-07-14 04:36:56 -0700

    helloooo need a console log to make sure its not just hanging

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.listen(3000, () => "Listening on 3000!");
 
 app.get('/', function (req, res) {
     res.sendFile('./index.html')
