ba49abd86a4937f1273d490aae30b2dc0a7043c4
Haejin <professionalhaejin@gmail.com>
2018-07-15 18:03:40 -0700

    holy crap...routes and css man. static assets man.

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.listen(3000, () => console.log("Listening on 3000!"));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/styles', function (req, res) {
+    res.sendFile(__dirname + '/main.css');
+});
